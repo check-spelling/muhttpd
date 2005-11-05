43c93284e9963d2f5ab616ddd1ee899617681a2e
inglorion <muhttpd@inglorion.net>
2005-11-05 10:59:12 +0000

    Improvements to configure script; bumped version to 1.0.5.
    git-svn-id: svn://svn.code.sf.net/p/muhttpd/code/current@29 7e1f86b9-3cf0-0310-9d02-db70efa590c4

--- flags.h
+++ flags.h
@@ -4,6 +4,6 @@
 #define ENABLE_LOGGING
 #define ENABLE_PIDFILE
 #define ENABLE_BACKGROUND
-#define VERSION 1.0.3
-#define SERVER_SOFTWARE "muhttpd/1.0.3"
+#define VERSION 1.0.5
+#define SERVER_SOFTWARE "muhttpd/1.0.5"
 #define CONFIGFILE "/etc/muhttpd/muhttpd.conf"
