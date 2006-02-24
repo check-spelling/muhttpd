290aa870227fd506176512de4c565cacc18224ba
inglorion <muhttpd@inglorion.net>
2006-02-24 08:56:28 +0000

    Setting environ = NULL to implement clearenv (as several sources suggest) causes a segmentation fault, at least on OpenBSD 3.8. I've reverted back to the old behavior of allocating a one-element array of char* and initializing the single element to NULL.
    The only difference between the new clearenv.c and the one from rev 33 
    (and before) is that this one checks if the malloc worked.
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/muhttpd/code/current@44 7e1f86b9-3cf0-0310-9d02-db70efa590c4

--- clearenv.c
+++ clearenv.c
@@ -1,12 +1,17 @@
 #include "flags.h"
 #include <stdlib.h>
 
-/*@-incondefs@*/
-/*@null@*/
 extern char **environ;
 
 int clearenv(void) {
-	environ = NULL;
+	/* We would like to free previously set environment variables here,
+	 * but at least FreeBSD 5.1 doesn't let us */
+
+	/* Create empty environment */
+	environ = malloc(sizeof(char*));
+	if(environ) {
+		*environ = NULL;
+	}
 
 	return 0;
 }
