697b47838e4fa4f927a141914aabd15dcf5ac0c8
inglorion <muhttpd@inglorion.net>
2009-11-20 08:31:32 +0000

    Increased version number to 1.1.5
    git-svn-id: svn://svn.code.sf.net/p/muhttpd/code/current@75 7e1f86b9-3cf0-0310-9d02-db70efa590c4

--- flags.h
+++ flags.h
@@ -5,6 +5,6 @@
 #define ENABLE_LOGGING
 #define ENABLE_PIDFILE
 #define ENABLE_SSL
-#define VERSION 1.1.4
-#define SERVER_SOFTWARE "muhttpd/1.1.4"
+#define VERSION 1.1.5
+#define SERVER_SOFTWARE "muhttpd/1.1.5"
 #define CONFIGFILE "/etc/muhttpd/muhttpd.conf"
